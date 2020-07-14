7d49e9843f40e2545d0bf9f6aba2d728cf3c4f26
junseno0 <339365405@qq.com>
2020-07-14 09:51:54 +0800

    algo/list: solution by fast and slow runners for finding common node in lists. v3.

--- find_common_node_in_lists/find_common_nodes_in_lists.cpp
+++ find_common_node_in_lists/find_common_nodes_in_lists.cpp
@@ -73,6 +73,52 @@ public:
     }
 };
 
+/*
+链接：https://www.nowcoder.com/questionTerminal/6ab1d9a29e88450685099d45c9e31e46?answerType=1&f=discussion
+来源：牛客网
+
+思路：首先我们要知道什么是公共节点，两个链表从某一节点开始，他们的next都指向同一个节点。
+但由于是单向链表的节点，每个节点只有一个next，因此从第一个公共节点开始，之后他们的所有节点都是重合的，不可能再出现分叉。
+所以可以先遍历两个链表得到他们的长度，就能知道哪个链表比较长，以及长的链表比短的链表多几个结点。
+在第二次遍历的时候，在较长的链表上先走若干步，接着同时在两个链表上遍历，找到的第一个相同的结点就是他们的第一个公共结点。
+*/
+class Solution_by_fast_slow_runners {
+public:
+    ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {
+        if(!pHead1 || !pHead2) return NULL;
+        ListNode *lt1 = pHead1;
+        ListNode *lt2 = pHead2;
+        ListNode *foundnode = NULL;
+        int size1 = 0;
+        int size2 = 0;
+        //for non-loop list
+        while(lt1) {
+            lt1 = lt1->next;
+            size1++;
+            if(lt1 == pHead1) break;
+        }
+        while(lt2) {
+            lt2 = lt2->next;
+            size2++;
+            if(lt2 == pHead2) break;
+        }
+        int diff = size1 > size2 ? size1 - size2 : size2 - size1;
+        lt1 = pHead1;
+        lt2 = pHead2;
+        if (size1 > size2) {
+            while(diff-- > 0) { lt1 = lt1->next;}
+        } else {
+            while(diff-- > 0) { lt2 = lt2->next;}
+        }
+        while(lt1 != lt2) {
+            lt1 = lt1->next;
+            lt2 = lt2->next;
+        }
+        foundnode = lt1;
+        return foundnode;
+    }
+};
+
 int main()
 {
     int n = 7;
@@ -98,14 +144,19 @@ int main()
 
     Solution_by_set *st = new Solution_by_set();
     ListNode *p = st->FindFirstCommonNode(header1, header2);
-    std::cout<<"found common node in list: "<<p<<"\n";
+    std::cout<<"found common node in list by set: "<<p<<"\n";
     delete st;
 
     Solution_by_compare *sc = new Solution_by_compare();
     p = sc->FindFirstCommonNode(header1, header2);
-    std::cout<<"found common node in list: "<<p<<"\n";
+    std::cout<<"found common node in list by comparision: "<<p<<"\n";
     delete sc;
-
+ 
+    Solution_by_fast_slow_runners *sr = new Solution_by_fast_slow_runners();
+    p = sr->FindFirstCommonNode(header1, header2);
+    std::cout<<"found common node in list by fast and slow runners: "<<p<<"\n";
+    delete sr;
+ 
     //delete list
     lt = header1;
     ListNode *tmp;
