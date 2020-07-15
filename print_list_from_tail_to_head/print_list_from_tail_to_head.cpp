3eb60660aa583eda5e806a0f715003804fbcfe5c
junseno0 <339365405@qq.com>
2020-07-15 09:22:54 +0800

    algo/list: solutions with reversion and recusion for printing list from tail to head.

--- print_list_from_tail_to_head/print_list_from_tail_to_head.cpp
+++ print_list_from_tail_to_head/print_list_from_tail_to_head.cpp
@@ -2,7 +2,7 @@
 #include <list>
 #include <algorithm>
 #include <vector>
-
+#include <stack>
 /*
 输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
 */
@@ -15,7 +15,7 @@ struct ListNode {
 };
 
 /*
-Recursion method.
+* Recursion method.
 */
 class Solution_by_recursion {
 public:
@@ -32,6 +32,113 @@ public:
         if(head == NULL) return vt;
         //recursion method
         ListFromTailToHead(head, vt);
+        return vt;
+    }
+};
+
+/*
+* Recursion method with 1 input para. TODO.
+*/
+class Solution_by_recursion_v2 {
+public:
+    std::vector<int> printListFromTailToHead(ListNode* head) {
+        //recursion method
+        std::vector<int> vt;
+        //check
+        if(head == NULL) return vt;
+        //recursion method
+        //TODO
+        return vt;
+    }
+};
+
+/*
+* Stack method.
+*/
+class Solution_by_stack {
+public:
+    std::vector<int> printListFromTailToHead(ListNode* head) {
+        //recursion method
+        std::vector<int> vt;
+        //check
+        if(head == NULL) return vt;
+        std::stack<int> st;
+        ListNode* lt = head;
+        while(lt) {
+            st.push(lt->val);
+            lt = lt->next;
+        }
+        while(!st.empty()) {
+            vt.push_back(st.top());
+            st.pop();
+        }
+        return vt;
+    }
+};
+
+/*
+* Reverse vector method.
+*/
+class Solution_by_reverse_vector {
+public:
+    std::vector<int> printListFromTailToHead(ListNode* head) {
+        //recursion method
+        std::vector<int> vt;
+        //check
+        if(head == NULL) return vt;
+        ListNode* lt = head;
+        while(lt) {
+            vt.push_back(lt->val);
+            lt = lt->next;
+        }
+        //reverse vector, possible STL:
+        //template< class BidirIt >
+        //void reverse( BidirIt first, BidirIt last );
+        int n = vt.size();
+        int i = 0;
+        int j = n - 1;
+        while(i < j) {
+            std::swap(vt[i], vt[j]);
+            ++i; --j;
+        }
+        return vt;
+    }
+};
+
+/*
+* Reverse list and push node val into vector.
+*/
+class Solution_by_reverse_list {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL) return NULL;
+        // 2 pointers. prev, curr
+        // store curr next one
+        // curr runs/updates from head to end, relink all the way behind it
+        //in the end, curr as null, prev as end of list
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        while(curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        //return node
+        return prev;
+    }
+    std::vector<int> printListFromTailToHead(ListNode* head) {
+        //recursion method
+        std::vector<int> vt;
+        //check
+        if(head == NULL) return vt;
+        ListNode *lt = head;
+        lt = reverseList(lt);
+        while(lt) {
+            vt.push_back(lt->val);
+            lt = lt->next;
+        }
         return vt;
     }
 };
@@ -85,7 +192,35 @@ int main()
     for(unsigned int i = 0; i < vt.size(); i++) {
         std::cout<<"["<<vt[i]<<"] ";
     }
+    std::cout<<"\n";
     delete sr;
+
+    Solution_by_stack *st = new Solution_by_stack();
+    std::vector<int> vs = st->printListFromTailToHead(header);
+    traverse(header);
+    for(unsigned int i = 0; i < vs.size(); i++) {
+        std::cout<<"["<<vs[i]<<"] ";
+    }
+    std::cout<<"\n";
+    delete st;
+
+    Solution_by_reverse_vector *sv = new Solution_by_reverse_vector();
+    std::vector<int> vv = sv->printListFromTailToHead(header);
+    traverse(header);
+    for(unsigned int i = 0; i < vv.size(); i++) {
+        std::cout<<"["<<vv[i]<<"] ";
+    }
+    std::cout<<"\n";
+    delete sv;
+
+    Solution_by_reverse_list *sl = new Solution_by_reverse_list();
+    std::vector<int> vl = sl->printListFromTailToHead(header);
+    traverse(header);//list itself is changed now!! u cannot delete it on heap now!!
+    for(unsigned int i = 0; i < vl.size(); i++) {
+        std::cout<<"["<<vl[i]<<"] ";
+    }
+    std::cout<<"\n";
+    delete sl;//cannot delete all nodes now.
 
     deleteList(header);
     return 0;
