bf1e30b5bd7a8234ef6a2156133d4f99bda0ad69
junseno0 <339365405@qq.com>
2020-08-02 10:31:38 +0800

    algo/backtrack: solution for num_of_islands.

--- num_of_islands/num_of_islands.cpp
+++ num_of_islands/num_of_islands.cpp
@@ -6,31 +6,16 @@
 #include <string>
 
 using namespace std;
-
 /*
-LC: 39
-给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
-candidates 中的数字可以无限制重复被选取。
-说明：
-所有数字（包括 target）都是正整数。
-解集不能包含重复的组合。 
-*/
+200. Number of Islands
 
-/*
-去重复
-在搜索的时候，需要设置搜索起点的下标 begin ，由于一个数可以使用多次，下一层的结点从这个搜索起点开始搜索；
-在搜索起点 begin 之前的数因为以前的分支搜索过了，所以一定会产生重复。
-剪枝提速
-如果一个数位搜索起点都不能搜索到结果，那么比它还大的数肯定搜索不到结果，基于这个想法，我们可以对输入数组进行排序，以减少搜索的分支；
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally
+or vertically. You may assume all four edges of the grid are all surrounded by water. 
 
-排序是为了提高搜索速度，非必要；
-
-搜索问题一般复杂度较高，能剪枝就尽量需要剪枝。把候选数组排个序，遇到一个较大的数，如果以这个数为起点都搜索不到结果，后面的数就更搜索不到结果了。
-
-作者：liweiwei1419
-链接：https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
 来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+链接：https://leetcode-cn.com/problems/number-of-islands
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 class Solution_DFS {
 private:
