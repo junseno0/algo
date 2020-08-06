ddc502ad24bf9a27d5ad0f9573bfce6725b01582
junseno0 <339365405@qq.com>
2020-08-06 10:31:12 +0800

    algo/array: add solution commnet for trapping_rain_water.

--- trapping_rain_water/coin_change.cpp
+++ trapping_rain_water/coin_change.cpp
@@ -81,6 +81,17 @@ public:
     }
 };
 
+/*
+从动态编程方法的示意图中我们注意到，只要 \text{right\_max}[i]>\text{left\_max}[i]right_max[i]>left_max[i] （元素 0 到元素 6），
+积水高度将由 left_max 决定，类似地 \text{left\_max}[i]>\text{right\_max}[i]left_max[i]>right_max[i]（元素 8 到元素 11）。
+所以我们可以认为如果一端有更高的条形块（例如右端），积水的高度依赖于当前方向的高度（从左到右）。当我们发现另一侧（右侧）的条形块高度不是最高的，我们则开始从相反的方向遍历（从右到左）。
+我们必须在遍历时维护 \text{left\_max}left_max 和 \text{right\_max}right_max ，但是我们现在可以使用两个指针交替进行，实现 1 次遍历即可完成。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
 class Solution_two_runner {
 public:
     int trap(vector<int>& height) {
